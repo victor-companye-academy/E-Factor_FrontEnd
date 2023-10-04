db64ff5d15feb6495ebd6bfd2d2dcf5b7ba37de3
Victor Stefano <victor_dev@companye.academy>
2023-10-04 19:58:53 -0300

    chore: importação de componentes e criação de variáveis

--- educ-front-efactor/src/app/app.module.ts
+++ educ-front-efactor/src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AddressComponent } from './shared/address/address.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SidebarComponent,
+    FooterComponent,
+    AddressComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
