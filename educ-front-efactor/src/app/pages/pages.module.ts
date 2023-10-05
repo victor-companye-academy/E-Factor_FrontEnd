de0f73d1f6eb5bd32f765e17c6555c3b31f92ac5
unknown <ericris_dev>
2023-10-04 22:20:33 -0300

    wip: Criação do componente footer

--- educ-front-efactor/src/app/pages/pages.module.ts
+++ educ-front-efactor/src/app/pages/pages.module.ts
@@ -4,15 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 
-
 @NgModule({
   declarations: [
-  
     HomeComponent
   ],
   imports: [
     CommonModule,
     PagesRoutingModule
+  
   ]
 })
 export class PagesModule { }
--- educ-front-efactor/src/app/shared/shared.module.ts
+++ educ-front-efactor/src/app/shared/shared.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddressComponent } from './address/address.component';
 import { CardComponent } from './card/card.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/FooterComponent';
 
 
 
@@ -12,12 +12,13 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   declarations: [
     AddressComponent,
     CardComponent,
-    FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [AddressComponent, CardComponent, HeaderComponent, SidebarComponent,FooterComponent]
 })
 export class SharedModule { }
