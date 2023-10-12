2ae645dbdf2a32a4e98ebe95598fd94de3d0f048
unknown <ericris_dev>
2023-10-11 21:21:34 -0300

    wip: Criação da interface 'card-vacacy'

---
+++ educ-front-efactor/src/app/core/interfaces/card-vacacy.ts
@@ -0,0 +1,13 @@
+export interface CardVacacy {
+    src: string | null;
+    title: string;
+
+    days: string;
+    status: string;
+
+    description: string;
+
+    locality: string;
+    skills: Array<string>;
+    mode: string;
+}
--- educ-front-efactor/src/app/shared/card-vacacy/card-vacacy.component.ts
+++ educ-front-efactor/src/app/shared/card-vacacy/card-vacacy.component.ts
@@ -8,6 +8,9 @@ import { Component, HostListener, Input } from '@angular/core';
 export class CardVacacyComponent {
 
   @Input({alias:'short'}) public isShort?: boolean;
+  @Input({alias:'cards'}) public cards?: boolean;
+
+
   private screenSize: number = 1000;
   public isSmallScreen: boolean = window.innerWidth <= this.screenSize;
 
