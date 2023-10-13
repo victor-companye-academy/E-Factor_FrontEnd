78f51fdb1c9d4247cf181e059c9219c0743f1f5d
unknown <ericris_dev>
2023-10-13 18:05:29 -0300

    refactor: Ajustes nas linhas e no peso da fonte

--- educ-front-efactor/src/app/shared/card-vacacy/card-vacacy.component.ts
+++ educ-front-efactor/src/app/shared/card-vacacy/card-vacacy.component.ts
@@ -10,14 +10,4 @@ export class CardVacacyComponent {
 
   @Input({ alias: 'short' }) public isShort?: boolean;
   @Input({ alias: 'cards' }) public cards?: Array<CardVacacy>;
-
-
-
-  private screenSize: number = 1000;
-  public isSmallScreen: boolean = window.innerWidth <= this.screenSize;
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.isSmallScreen = window.innerWidth <= this.screenSize;
-  }
 }
