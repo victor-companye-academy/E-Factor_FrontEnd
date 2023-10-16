cb8b647bc4bdbd0e100cee0508d7db69c855150d
unknown <ericris_dev>
2023-10-16 11:21:23 -0300

    feat: Interface do professional criada

---
+++ educ-front-efactor/src/app/core/interfaces/card-professional.ts
@@ -0,0 +1,7 @@
+export interface CardProfessional {
+    userName: string;
+    title: string;
+    description:string;
+    locality:string;
+    skills:Array<string>;
+}
--- educ-front-efactor/src/app/pages/home/home.component.ts
+++ educ-front-efactor/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardDetails } from 'src/app/core/interfaces/card-details';
+import { CardProfessional } from 'src/app/core/interfaces/card-professional';
 import { CardVacacy } from 'src/app/core/interfaces/card-vacacy';
 
 @Component({
@@ -41,9 +42,34 @@ export class HomeComponent {
 
   protected cardVacacy: Array<CardVacacy> = [
     {
-      companyName: 'Google', title: 'Desenvolvedor Senior', days: 'Há 6 dias', contract: 'CLT', locality: 'São Paulo - SP', description: 'Lorem ipsum dolor sit amet consectetur. Suspendisse maecenas condimentum blandit vitae tortor dis nec mattis. Ipsum eu praesent orci lacus. Sed lorem id duis tempor felis tempor molestie. Commodo nisl sed massa varius magna interdum. Pulvinar nibh dignissim egestas ut. Nibh amet vitae sed duis sit diam. Feugiat sit viverra habitant platea adipiscing amet in nisi cursus. Feugiat mi rutrum velit vitae.', mode: 'Home Office', skills: ['Angular', 'Javascript', 'Typescript', 'Spring'], status: 'Ultimas Vagas', 
+      companyName: 'Google',
+      title: 'Desenvolvedor Senior',
+      days: 'Há 6 dias',
+      contract: 'CLT',
+      locality: 'São Paulo - SP',
+      description: 'Lorem ipsum dolor sit amet consectetur. Suspendisse maecenas condimentum blandit vitae tortor dis nec mattis. Ipsum eu praesent orci lacus. Sed lorem id duis tempor felis tempor molestie. Commodo nisl sed massa varius magna interdum. Pulvinar nibh dignissim egestas ut. Nibh amet vitae sed duis sit diam. Feugiat sit viverra habitant platea adipiscing amet in nisi cursus. Feugiat mi rutrum velit vitae.',
+      mode: 'Home Office',
+      skills: ['Angular', 'Javascript', 'Typescript', 'Spring'],
+      status: 'Ultimas Vagas',
     },
     {
-      companyName: 'Google', title: 'Desenvolvedor Senior', days: 'Há 6 dias', contract: 'CLT', locality: 'São Paulo - SP', description: 'Lorem ipsum dolor sit amet consectetur. Suspendisse maecenas condimentum blandit vitae tortor dis nec mattis. Ipsum eu praesent orci lacus. Sed lorem id duis tempor felis tempor molestie. Commodo nisl sed massa varius magna interdum. Pulvinar nibh dignissim egestas ut. Nibh amet vitae sed duis sit diam. Feugiat sit viverra habitant platea adipiscing amet in nisi cursus. Feugiat mi rutrum velit vitae.', mode: 'Home Office', skills: ['Angular', 'Javascript', 'Typescript', 'Spring'], status: 'Ultimas Vagas', src: 'https://logosmarcas.net/wp-content/uploads/2020/09/Google-Logo.png'
+      companyName: 'Google',
+      title: 'Desenvolvedor Senior',
+      days: 'Há 6 dias',
+      contract: 'CLT',
+      locality: 'São Paulo - SP',
+      description: 'Lorem ipsum dolor sit amet consectetur. Suspendisse maecenas condimentum blandit vitae tortor dis nec mattis. Ipsum eu praesent orci lacus. Sed lorem id duis tempor felis tempor molestie. Commodo nisl sed massa varius magna interdum. Pulvinar nibh dignissim egestas ut. Nibh amet vitae sed duis sit diam. Feugiat sit viverra habitant platea adipiscing amet in nisi cursus. Feugiat mi rutrum velit vitae.',
+      mode: 'Home Office',
+      skills: ['Angular', 'Javascript', 'Typescript', 'Spring'],
+      status: 'Ultimas Vagas', src: 'https://logosmarcas.net/wp-content/uploads/2020/09/Google-Logo.png'
+    }]
+
+  protected cardProfessional: Array<CardProfessional> = [
+    {
+      userName: 'Marcos Henrique',
+      title: 'Desenvolvedor Senior',
+      locality: 'São Paulo, SP',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...',
+      skills: ['Phyton', 'Javascript', 'Typescript', 'Spring', 'React', 'PHP', 'HTML']
     }]
 }
