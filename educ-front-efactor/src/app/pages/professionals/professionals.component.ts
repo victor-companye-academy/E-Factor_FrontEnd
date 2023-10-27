e93bebe30d1a8da37cbeab3fe050692cb1987ba4
ericris_dev <ericris_dev@companye.academy>
2023-10-27 11:58:49 -0300

    config: Primeng configurado

--- educ-front-efactor/src/app/pages/professionals/professionals.component.ts
+++ educ-front-efactor/src/app/pages/professionals/professionals.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-professionals',
   templateUrl: './professionals.component.html',
   styleUrls: ['./professionals.component.scss']
 })
-export class ProfessionalsComponent {
+export class ProfessionalsComponent implements OnInit{
+  cities: City[] | undefined;
+    
+  formGroup: FormGroup | undefined | any;
 
+  ngOnInit() {
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+          { name: 'Istanbul', code: 'IST' },
+          { name: 'Paris', code: 'PRS' }
+      ];
+
+      this.formGroup = new FormGroup({
+          selectedCity: new FormControl<City | null>(null)
+      });
+  }
 }
