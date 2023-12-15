9979334579ba304cc25b1a67ee9287ebe96cfb21
ericris_dev <ericris_dev@companye.academy>
2023-12-15 20:29:56 -0300

    style: Ajuste no card de vaga

--- src/app/shared/card-vacancy/card-vacancy.component.ts
+++ src/app/shared/card-vacancy/card-vacancy.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Vacancy } from 'src/app/core/interfaces/vacancy';
+import { formattedDate } from 'src/app/core/utils/formattedDate';
 
 @Component({
   selector: 'card-vacancy',
@@ -7,7 +8,30 @@ import { Vacancy } from 'src/app/core/interfaces/vacancy';
   styleUrls: ['./card-vacancy.component.scss']
 })
 export class CardVacancyComponent {
-
   @Input({ alias: 'short' }) public isShort?: boolean;
-  @Input({ alias: 'card' }) public card?:Vacancy;
+  @Input({ alias: 'card' }) public card?: Vacancy;
+
+  onDays(): string {
+    const date = this.card?.createDate;
+
+    const year = parseInt(date?.slice(6) as string);
+    const month = parseInt(date?.slice(3, 5) as string) - 1;
+    const day = parseInt(date?.slice(0, 2) as string);
+
+    // const days = (1000 * 60 * 60 * 24);
+
+    console.log('Data da vaga: ' + date)
+    console.log('Data de hoje: ' + formattedDate(new Date()))
+
+    const decimalNumber = (new Date().getTime() - new Date(year, month, day).getTime() ) / (1000 * 60 * 60 * 24)
+    const integerNumber = Math.floor(decimalNumber)
+
+    console.log(Math.floor(integerNumber))
+
+    return ''
+  }
+
+  ngOnInit() {
+    this.onDays()
+  }
 }
